c2877a1cfb337c83c4b8f12da5b520c21e5c77f5
Yeonjune0408 <yeonjnuekim0408@gmail.com>
2026-09-17 21:42:59 +0900

    more accurate & diverse angle

--- rust/sincostan/src/main.rs
+++ rust/sincostan/src/main.rs
@@ -10,7 +10,6 @@ fn calculate_trig_values(angle_degrees: f64) -> (f64, f64, f64) {
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read input");
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
             let n: i64 = match input.trim().parse() {
@@ -23,20 +22,26 @@ fn main() {
 
             match n {
                 15 => println!("sin: (√6-√2)/4, cos: (√6+√2)/4, tan: 2-√3"),
+                18 => println!("sin: (√5-1)/4, cos: √(10+2√5)/4, tan: √(25-10√5)/5"),
                 30 => println!("sin: 1/2, cos: √3/2, tan: √3/3"),
+                36 => println!("sin: (√10-2√5)/4, cos: (1+√5)/4, tan: √(5-2√5)"),
                 45 => println!("sin: √2/2, cos: √2/2, tan: 1"),
+                54 => println!("sin: (1+√5)/4, cos: √(10-2√5)/4, tan: √(25+10√5)/5"),
                 60 => println!("sin: √3/2, cos: 1/2, tan: √3"),
                 75 => println!("sin: (√6+√2)/4, cos: (√6-√2)/4, tan: 2+√3"),
+                72 => println!("sin: √(10+2√5)/4, cos: (√5-1)/4, tan: √(5+2√5)"),
                 90 => println!("sin: 1, cos: 0, tan: ∞"),
+                108 => println!("sin: √(10+2√5)/4, cos: -(√5-1)/4, tan: -√(5+2√5)"),
                 120 => println!("sin: √3/2, cos: -1/2, tan: -√3"),
+                144 => println!("sin: √(10-2√5)/4, cos: -(1+√5)/4, tan: -√(5-2√5)"),
                 150 => println!("sin: 1/2, cos: -√3/2, tan: -√3/3"),
                 0 => println!("sin: 0, cos: 1, tan: 0"),
                 180 => println!("sin: 0, cos: -1, tan: 0"),
-                210 => println!("sin: -√3/2, cos: -1/2, tan: -√3/3"),
-                240 => println!("sin: -1/2, cos: -√3/2, tan: -√3"),
+                210 => println!("sin: -1/2, cos: -√3/2, tan: √3/3"),
+                240 => println!("sin: -√3/2, cos: -1/2, tan: √3"),
                 270 => println!("sin: -1, cos: 0, tan: ∞"),
-                300 => println!("sin: -√3/2, cos: 1/2, tan: √3"),
-                330 => println!("sin: -1/2, cos: √3/2, tan: √3/3"),
+                300 => println!("sin: -√3/2, cos: 1/2, tan: -√3"),
+                330 => println!("sin: -1/2, cos: √3/2, tan: -√3/3"),
                 360 => println!("sin: 0, cos: 1, tan: 0"),
                 _ => {
                     let (sin_value, cos_value, tan_value) = calculate_trig_values(n as f64);
